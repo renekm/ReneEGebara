48124188378a107425595747455546865bdc3bc0
eduardogebara <dugebara@uol.com.br>
2016-04-26 23:32:34 -0300

    Update ep3.py

--- ep3.py
+++ ep3.py
@@ -4,9 +4,6 @@ Created on Wed Apr 20 08:58:03 2016
 
 @author: Rene Martinez
 """
-import numpy as np 
-
-
 class Jogo:
     
         
@@ -17,7 +14,6 @@ class Jogo:
    
         
     def recebe_jogada (self, linha, coluna):
-        print("wweee")
         if self.jogadas %2 == 0:
             self.M[linha][coluna] == "O"
             self.jogadas += 1
@@ -27,7 +23,8 @@ class Jogo:
 
         
     def verifica_ganhador(self):
-        if self.jogadas >= 4:
+        if self.jogadas >= 4 and self.jogadas<9:
+            
             if self.M[0][0] == "X" and self.M[0][1] == "X" and self.M[0][2] == "X":
                 return 1 
             elif self.M[1][0] == "X" and self.M[1][1] == "X" and self.M[1][2] == "X":
@@ -36,8 +33,15 @@ class Jogo:
                 return 1
             elif self.M[0][0] == "X" and self.M[1][1] == "X" and self.M[2][2] == "X":
                 return 1
+            elif self.M[0][0] == "X" and self.M [1][0] == "X" and self.M[2][0] == "X":
+                return 1
+            elif self.M[0][1] == "X" and self.M[1][1] == "X" and self.M[2][1] == "X":
+                return 1
+            elif self.M[0][2] == "X" and self.M[1][2] == "X" and self.M[2][2] == "X":
+                return 1    
             elif self.M[0][2] == "X" and self.M[1][1] == "X" and self.M[2][0] == "X":
                 return 1
+            
             elif self.M[0][0] == "O" and self.M[0][1] == "O" and self.M[0][2] == "O":
                 return 2
             elif self.M[1][0] == "O" and self.M[1][1] == "O" and self.M[1][2] == "O":
@@ -48,23 +52,22 @@ class Jogo:
                 return 2
             elif self.M[0][2] == "O" and self.M[1][1] == "O" and self.M[2][0] == "O":
                 return 2
+            elif self.M[0][0] == "O" and self. M [1][0] == "O" and self.M[2][0] == "O":
+                return 2
+            elif self.M[0][1] == "O" and self.M[1][1] == "O" and self.M[2][1] == "O":
+                return 2
+            elif self.M[0][2] == "O" and self.M[1][2] == "O" and self.M[2][2] == "O":
+                return 2    
+            
         elif self.jogadas == 9:
-            return 0
+                return 0
                 
         else:
             return -1
+
     
     def limpa_jogadas(verifica_ganhador):
         a = verifica_ganhador()
         if a == 1 or a == 2 or a == 0:
-            return Jogo
-
-
-
-                
-                
-            
-            
-        
-        
-        
+            self.M = [[1,2,3], [4,5,6], [7,8,9]]
+            self.jogadas=0
